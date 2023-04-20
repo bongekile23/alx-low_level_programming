bcea1a1ce428bf43e0838ffe0e10881dc51c4aaf
bongekile23 <nkosibl8@gmail.com>
2023-04-20 12:24:21 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stfio.h>
+#include <stdio.>
 /**
- * main -Entry point
- * Return:0
+ * main - Entry point
+ * Return 
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+puts("/"Programming is like building a multilingual puzzle");
+return(0);
 }
 
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that uses printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammer,but the outcomeis a piece of art,\n")
+return (0);
+}
