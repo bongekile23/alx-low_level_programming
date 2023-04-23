41d1109ffa7d0bcfddfe420a4d944fe1d7ac1ea2
bongekile23 <nkosibl8@gmail.com>
2023-04-23 07:32:15 -0700

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase then new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i=97; i = 123; i++)
+	{
+		if (i !=101 && i != 113);
+
---
+++ 0x01-variables_if_else_while/print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase then new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+        int i;
+
+        for (i=97; i = 123; i++)
+        {
+                if (i !=101 && i != 113);
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
