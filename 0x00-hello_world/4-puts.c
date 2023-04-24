44d49899ef2db9d55cceb07b0965b215f1de5bd6
bongekile23 <nkosibl8@gmail.com>
2023-04-24 12:42:39 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - A progam that uses the print function
  * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\" "Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
