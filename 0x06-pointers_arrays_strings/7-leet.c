5a403cf6c40085939ec1e8962a4359fd447e0d78
bongekile23 <nkosibl8@gmail.com>
2023-05-04 11:55:09 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,44 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-
- * leet - encode into 1337speak
-
- * @n: input value
-
- * Return: n value
-
+ * leet - change vowels to numbers.
+ * @s: analized string.
+ *
+ * Return: String with all vowels changed.
  */
-
-char *leet(char *n)
-
+char *leet(char *s)
 {
-
-        int i, j;
-
-        char s1[] = "aAeEoOtTlL";
-
-        char s2[] = "4433007711";
-
-
-        for (i = 0; n[i] != '\0'; i++)
-
-        {
-
-                for (j = 0; j < 10; j++)
-
-                {
-
-                        if (n[i] == s1[j])
-
-                        {
-
-                                n[i] = s2[j];
-
-                        }
-
-                }
-
-        }
-
-        return (n);
+	char a[] = "aeotlAEOTL";
+	char n[] = "4307143071";
+	int i = 0;
+	int j;
+	
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j <= 9; j++)
+		{
+		if (*(s + i) == a[j])
+		{
+		*(s + i) = n[j];
+		}
+		}
+		i++;
+	}
+		return (s);
+}
