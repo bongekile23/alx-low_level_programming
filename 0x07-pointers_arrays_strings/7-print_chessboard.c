4aac8f82f347bc76f2eea0efaccd0686ddbfcfcd
bongekile23 <nkosibl8@gmail.com>
2023-05-08 10:40:04 -0700

    strspn

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,15 +5,15 @@
  * @a: array
  * Return: Always 0 (success)
  */
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
 	int i;
 	int j;
 
 	for (i = 0; i < 8; i++)
 	{
-	for (i = 0; j < 8; j++)
-		_putchar(a[i][j]);
-	_putchar('\n');
+		for (j = 0; j < 8; j++)
+			_putchara([i][j]);
+		_putchar('\n');
 	}
 }
