067f8c0c03a5463fa10b9e3c694dd46f471396f8
bongekile23 <nkosibl8@gmail.com>
2023-05-18 13:36:49 -0700

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,15 +9,20 @@
  */
 int *array_range(int min, int max)
 {
-	int *ar;
-	int i;
+	int *ptr;
+	int i, size;
 
 	if (min > max)
 		return (NULL);
-	ar = malloc(sizeof(*ar) * ((max - min) + 1);
-	if (ar == NULL)
+
+	size = max - min + 1;
+
+	ptr = malloc(sizeof(int) * size);
+
+	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; min <= max; i++; min++)
-	a[i] = min;
-	return (ar);
+
+	for (i = 0; min <= max; i++)
+	ptr[i] = min++;
+	return (ptr);
 }
