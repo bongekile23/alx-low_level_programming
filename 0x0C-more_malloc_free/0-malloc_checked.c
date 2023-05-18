33d5355c0740072fb4bcbb87b9348eddb388f731
bongekile23 <nkosibl8@gmail.com>
2023-05-18 06:51:12 -0700

    malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked(unsigned int b)
+ * @b: number of bytes ro allocate
+ * *Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
